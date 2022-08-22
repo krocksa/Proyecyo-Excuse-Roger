8ef045d1c8e4ff6f42fe59b7141575dd78d76a3a
krocksa <rogeryonatan@gmail.com>
2022-08-22 23:42:13 +0000

    Proyecto Listerine

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+function randomNumber(array) {
+  return Math.floor(Math.random() * array.length);
+}
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let excusa = document.getElementById("excuse");
+  excusa.innerHTML =
+    who[randomNumber(who)] +
+    " " +
+    what[randomNumber(what)] +
+    " " +
+    when[randomNumber(when)];
 };
